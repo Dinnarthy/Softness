e0de78f37304de97ea19219b32c13b8e3f27e27b
Dinnarthy <Dinnarthy@Dinnarthy-PC>
2014-04-13 12:57:41 -0300

    Segundo Commit. Cliente.java e Endereco.java

--- Softness/src/br/com/softness/cliente/Cliente.java
+++ Softness/src/br/com/softness/cliente/Cliente.java
@@ -6,52 +6,163 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import br.com.softness.endereco.Endereco;
+
 @Entity
-@Table(name="cliente")
+@Table(name = "cliente")
 public class Cliente {
-	
+
 	@Id
 	@GeneratedValue
 	private Integer idCliente;
-	
-	@Column(name="nome", nullable=false)
+
+	@Column(name = "nome", nullable = false)
 	private String nome;
-	
-	@Column(name="cpf")
+
+	@Column(name = "cpf")
 	private String cpf;
-	
-	@Column(name="rg")
+
+	@Column(name = "rg")
 	private String rg;
-	
-	@Column(name="orgao_emissor")
+
+	@Column(name = "orgao_emissor")
 	private String orgaoEmissor;
-	
-	@Column(name="sexo")
+
+	@Column(name = "sexo")
 	private String sexo;
-	
-	@Column(name="estado_civil")
+
+	@Column(name = "estado_civil")
 	private String estadoCivil;
-	
+
 	@Temporal(TemporalType.DATE)
-	@Column(name="data_nascimento")
+	@Column(name = "data_nascimento")
 	private Date dataNacimento;
-	
-	@Column(name="numero_residencial")
+
+	@Column(name = "numero_residencial")
 	private String numeroResidencialString;
-	
-	@Column(name="numero_celular1")
+
+	@Column(name = "numero_celular1")
 	private String numeroCelular1;
-	
-	@Column(name="numero_celular2")
+
+	@Column(name = "numero_celular2")
 	private String numeroCelular2;
-	
-	@Column(name="email")
+
+	@Column(name = "email")
 	private String email;
-	
+
+	@OneToOne
+	@JoinColumn(name = "endereco_id")
+	Endereco endereco;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getOrgaoEmissor() {
+		return orgaoEmissor;
+	}
+
+	public void setOrgaoEmissor(String orgaoEmissor) {
+		this.orgaoEmissor = orgaoEmissor;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public Date getDataNacimento() {
+		return dataNacimento;
+	}
+
+	public void setDataNacimento(Date dataNacimento) {
+		this.dataNacimento = dataNacimento;
+	}
+
+	public String getNumeroResidencialString() {
+		return numeroResidencialString;
+	}
+
+	public void setNumeroResidencialString(String numeroResidencialString) {
+		this.numeroResidencialString = numeroResidencialString;
+	}
+
+	public String getNumeroCelular1() {
+		return numeroCelular1;
+	}
+
+	public void setNumeroCelular1(String numeroCelular1) {
+		this.numeroCelular1 = numeroCelular1;
+	}
+
+	public String getNumeroCelular2() {
+		return numeroCelular2;
+	}
+
+	public void setNumeroCelular2(String numeroCelular2) {
+		this.numeroCelular2 = numeroCelular2;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 	
 	
 
---
+++ Softness/src/br/com/softness/endereco/Endereco.java
@@ -0,0 +1,104 @@
+package br.com.softness.endereco;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+	@Id
+	@GeneratedValue
+	private Integer idEndereco;
+
+	@Column(name = "cep")
+	private Integer cep;
+
+	@Column(name = "estado")
+	private String estado;
+
+	@Column(name = "cidade")
+	private String cidade;
+
+	@Column(name = "bairro")
+	private String bairro;
+
+	@Column(name = "tipo_logradouro")
+	private String tipoLogradouro;
+
+	@Column(name = "logradouro")
+	private String logradouro;
+
+	@Column(name = "numero")
+	private String numero;
+
+	public Integer getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Integer idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public Integer getCep() {
+		return cep;
+	}
+
+	public void setCep(Integer cep) {
+		this.cep = cep;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getTipoLogradouro() {
+		return tipoLogradouro;
+	}
+
+	public void setTipoLogradouro(String tipoLogradouro) {
+		this.tipoLogradouro = tipoLogradouro;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+
+}
